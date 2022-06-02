abe90eb64cdeb57a7d5d9deabbfeb44e3b7b2fc4
Chris Sev <chris@better.dev>
2022-06-02 10:54:36 -0700

    moar

--- packages/sample/aquarium/aquarium.js
+++ packages/sample/aquarium/aquarium.js
@@ -1,5 +1,5 @@
 async function main(args) {
-  const res = fetch("http://functionschallenge.com/api/sammy");
+  const res = fetch("https://functionschallenge.com/api/sammy");
   const data = await res.json();
   return { sharks: data };
 }
